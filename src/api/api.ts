2cabbc72ae32bba68ae0d6792cdbe7c9184a1295
Kyle Tennison <kyletennison05@gmail.com>
2024-06-08 10:23:07 -0700

    chore: remove local cookie delete

--- src/api/api.ts
+++ src/api/api.ts
@@ -26,14 +26,6 @@ function getCookie(name: string): string | null {
 }
 
 /**
- * Erases a cookie from the browser
- * @param name The name of the cookie
- */
-function eraseCookie(name: string) {
-  document.cookie = name + "=; Max-Age=0";
-}
-
-/**
  * Checks if a user is logged in
  * @returns true if the user is logged in, false otherwise
  */
@@ -99,7 +91,6 @@ export async function logOut(): Promise<boolean> {
     return false;
   }
 
-  eraseCookie("polybrain-session");
   window.location.replace(`${API_BASE}auth0/logout`);
 
   return true;
